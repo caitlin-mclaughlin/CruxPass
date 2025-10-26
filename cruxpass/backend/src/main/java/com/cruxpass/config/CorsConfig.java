3580584346a441a74ff3d336b27b28e767c8dcf3
caitlin-mclaughlin <ctlnmclaughlin@gmail.com>
2025-10-26 13:58:09 -0600

    updated allowable origin

--- cruxpass/backend/src/main/java/com/cruxpass/config/CorsConfig.java
+++ cruxpass/backend/src/main/java/com/cruxpass/config/CorsConfig.java
@@ -3,7 +3,6 @@ package com.cruxpass.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.*;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -14,12 +13,13 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://cruxpass-frontend.onrender.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/api/**", config);
-        source.registerCorsConfiguration("/ws/**", config); // <--- allow websocket
+        source.registerCorsConfiguration("/ws/**", config);
 
         return new CorsFilter(source);
     }
